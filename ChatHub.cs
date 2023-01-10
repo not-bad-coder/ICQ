071d7174ce0c7cc4517c7551290391f3b3c5e142
Sonin Timur <sonintimur@gmail.com>
2023-01-11 00:35:01 +0300

    Добавьте файлы проекта.

---
+++ ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRApp {
+    public class ChatHub : Hub {
+        public async Task Send(string message, string userName) {
+            await Clients.All.SendAsync("Send", message, userName);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SignalRApp {
+    public class Startup {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app) {
+            app.UseDeveloperExceptionPage();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
